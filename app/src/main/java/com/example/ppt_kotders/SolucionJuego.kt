d7ee57b3c800e148ad85b7a207e6b3a13eb06b59
CiJuBar <101054409+CiJuBar@users.noreply.github.com>
2023-11-12 22:54:30 +0100

    Solucion juego

--- app/src/main/java/com/example/ppt_kotders/SolucionJuego.kt
+++ app/src/main/java/com/example/ppt_kotders/SolucionJuego.kt
@@ -1,6 +1,8 @@
 package com.example.ppt_kotders
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SolucionJuego : AppCompatActivity() {
@@ -9,8 +11,41 @@ class SolucionJuego : AppCompatActivity() {
 
         val idUser = intent.getIntExtra("Jugador_ID",-1)
         val condicion = intent.getStringExtra("Resultado")
+
+        // Condicionantes de logout
         if(condicion == null || idUser == -1){
+            if(idUser == -1){
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            } else{
+                val intent = Intent(this,Menu::class.java)
+                intent.putExtra("Jugador_ID",idUser)
+                startActivity(intent)
+            }
         }
-        setContentView(R.layout.activity_win)
+
+        val buttonD = findViewById<Button>(R.id.buttonD)
+        val buttonV = findViewById<Button>(R.id.buttonV)
+
+        if(condicion == "v") {
+            setContentView(R.layout.activity_win)
+            buttonV.setOnClickListener(){
+
+                val intent = Intent(this,Menu::class.java)
+                intent.putExtra("Jugador_ID",idUser)
+                startActivity(intent)
+            }
+            // Declarar contenidos
+        }
+
+        if(condicion == "d"){
+            setContentView(R.layout.activity_lose)
+
+            val intent = Intent(this,Menu::class.java)
+            intent.putExtra("Jugador_ID",idUser)
+            startActivity(intent)
+
+        }
+
     }
 }
