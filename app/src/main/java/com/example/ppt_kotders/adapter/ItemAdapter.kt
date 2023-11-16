0d75439bcc84f5a9ef4ea013bc4de949bf23ad0c
Daniel Benitez <danielbentez@gmail.com>
2023-11-16 11:56:59 +0100

    v.1.0

--- app/src/main/java/com/example/ppt_kotders/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/ppt_kotders/adapter/ItemAdapter.kt
@@ -3,14 +3,14 @@ package com.example.ppt_kotders.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.ppt_kotders.JuegoModelo
+import com.example.ppt_kotders.models.JuegoModelo
 import com.example.ppt_kotders.R
 
 class ItemAdapter(val list : List<JuegoModelo>): RecyclerView.Adapter<ItemViewHolder>(){
     // Recibe la lista que se creara en el historico
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val layoutinflater = LayoutInflater.from(parent.context)
-        return ItemViewHolder(layoutinflater.inflate(R.layout.item, parent,false))
+        return ItemViewHolder(layoutinflater.inflate(R.layout.activity_item, parent,false))
     }
 
     override fun getItemCount(): Int {
