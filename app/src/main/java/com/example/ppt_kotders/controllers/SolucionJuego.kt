c3e50a4475c17738bf38be5abd9ff23f4a8bc59c
Daniel Benitez <danielbentez@gmail.com>
2023-12-02 19:25:18 +0100

    v.1.3

--- app/src/main/java/com/example/ppt_kotders/controllers/SolucionJuego.kt
+++ app/src/main/java/com/example/ppt_kotders/controllers/SolucionJuego.kt
@@ -6,6 +6,7 @@ import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
@@ -26,11 +27,14 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStream
 import android.net.Uri
+import android.view.View
+import java.util.TimeZone
 
 
 class SolucionJuego : AppCompatActivity() {
 
-    private val CALENDAR_REQUEST_CODE = 123
+    private val CALENDAR_WRITE_PERMISSION_REQUEST_CODE = 101
+    private val CALENDAR_READ_PERMISSION_REQUEST_CODE = 102
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,172 +76,145 @@ class SolucionJuego : AppCompatActivity() {
             finish()
         }
 
-        buttonScreenshot.setOnClickListener {
-            // Verificar permisos de calendario
-            checkCalendarPermission()
-        }
-    }
+        // Función para guardar la captura en la galeria de imagenes
+        fun saveMediaToStorage(bitmap: Bitmap) {
+            val nombrearchivo = "${System.currentTimeMillis()}.jpg"
+            var fos: OutputStream? = null
 
-    fun saveVictoryInCalendar() {
 
-        val calendarId = 1 
-        val startTimeMillis = System.currentTimeMillis()
-        val endTimeMillis = startTimeMillis + 3600000
-
-        try {
-            val values = ContentValues().apply {
-                put(CalendarContract.Events.DTSTART, startTimeMillis)
-                put(CalendarContract.Events.DTEND, endTimeMillis)
-                put(CalendarContract.Events.TITLE, "Victoria en Piedra, Papel o Tijeras by Kotders")
-                put(CalendarContract.Events.CALENDAR_ID, calendarId)
-                put(CalendarContract.Events.DESCRIPTION, "¡Enhorabuena, has ganado!")
+            // Determinar la ruta de almacenamiento
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                this.contentResolver?.also { resolver ->
+                    val contentValues = ContentValues().apply {
+                        put(MediaStore.MediaColumns.DISPLAY_NAME, nombrearchivo)
+                        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                        put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                    }
+                    val imageUri: android.net.Uri? =
+                        resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                    fos = imageUri?.let { resolver.openOutputStream(it) }
+                }
+            } else {
+                val imagesDir =
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                val image = File(imagesDir, nombrearchivo)
+                fos = FileOutputStream(image)
             }
-            val uri: Uri? = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values)
 
-            // Comprueba si la inserción fue exitosa
-            if (uri == null) {
-                Log.e("SolucionJuego", "Error al insertar evento en el calendario.")
+            fos?.use {
+                // Comprimir y guardar la captura en la galeria
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+                Toast.makeText(
+                    this@SolucionJuego,
+                    "Captura realizada y guardada en la galería",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("SolucionJuego", "Excepción al insertar evento en el calendario: ${e.message}")
         }
 
-        runOnUiThread {
-            Toast.makeText(
-                this@SolucionJuego,
-                "Evento añadido al calendario",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
+        fun takeScreenshot() {
+            // Obtener la vista raíz de la actividad
+            val rootView = window.decorView.rootView
 
-    fun takeScreenshotAndSave() {
-        // Tomar captura de pantalla
-        val bitmap = takeScreenshot(this@SolucionJuego)
+            // Tomar captura de pantalla
+            val bitmap = Bitmap.createBitmap(rootView.width, rootView.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            rootView.draw(canvas)
 
-        // Guardar captura de pantalla en la galería
-        if (bitmap != null) {
+            // Guardar captura de pantalla en la galería
             saveMediaToStorage(bitmap)
         }
 
-        // Guardar victoria en el calendario
-        saveVictoryInCalendar()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults) // Llama a la implementación de la clase base
-
-        when (requestCode) {
-            CALENDAR_REQUEST_CODE -> {
-                // Verifica si ambos permisos fueron concedidos
-                val writeCalendarPermissionGranted =
-                    grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                val readCalendarPermissionGranted =
-                    grantResults.size > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED
-
-                if (writeCalendarPermissionGranted && readCalendarPermissionGranted) {
-                    // Ambos permisos concedidos, ahora puedes realizar operaciones relacionadas con el calendario
-                    takeScreenshotAndSave()
-                } else {
-                    // Al menos uno de los permisos fue denegado, puedes informar al usuario o realizar otra acción
-                    Toast.makeText(
-                        this@SolucionJuego,
-                        "Permisos de calendario denegados",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+        fun insertEventToCalendar() {
+            // Verificar permiso de escritura en calendario
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.WRITE_CALENDAR
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.WRITE_CALENDAR), CALENDAR_WRITE_PERMISSION_REQUEST_CODE
+                )
+                return
             }
-            else -> {
-                // Otros casos
+
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.READ_CALENDAR
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.READ_CALENDAR), CALENDAR_READ_PERMISSION_REQUEST_CODE
+                )
+                return
             }
-        }
-    }
 
-    private fun checkCalendarPermission() {
-        val writeCalendarPermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.WRITE_CALENDAR
-        )
-        val readCalendarPermission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.READ_CALENDAR
-        )
-
-        if (writeCalendarPermission != PackageManager.PERMISSION_GRANTED ||
-            readCalendarPermission != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Los permisos no están aceptados.
-            requestCalendarPermission()
-        } else {
-            // Los permisos están aceptados.
-            takeScreenshotAndSave()
-        }
-    }
-
-    private fun requestCalendarPermission() {
-        val permissions = arrayOf(
-            Manifest.permission.WRITE_CALENDAR,
-            Manifest.permission.READ_CALENDAR
-        )
-
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0]) ||
-            ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[1])
-        ) {
-            // El usuario ya ha rechazado el permiso anteriormente, debemos informarle que vaya a ajustes.
-        } else {
-            // El usuario nunca ha aceptado ni rechazado, así que le pedimos que acepte los permisos.
-            ActivityCompat.requestPermissions(
-                this,
-                permissions,
-                CALENDAR_REQUEST_CODE
+            // Obtener el ID del calendario predeterminado de Google
+            val projection = arrayOf(
+                CalendarContract.Calendars._ID, CalendarContract.Calendars.ACCOUNT_TYPE
             )
-        }
-    }
+            val selection =
+                "${CalendarContract.Calendars.IS_PRIMARY} = 1 AND ${CalendarContract.Calendars.ACCOUNT_TYPE} = ?"
+            val selectionArgs = arrayOf("com.google")
+            val cursor = this.contentResolver.query(
+                CalendarContract.Calendars.CONTENT_URI, projection, selection, selectionArgs, null
+            )
 
-    private fun takeScreenshot(activity: Activity): Bitmap {
-        val v1 = activity.window.decorView.rootView
-        v1.isDrawingCacheEnabled = true
-        val bitmap = Bitmap.createBitmap(v1.drawingCache)
-        v1.isDrawingCacheEnabled = false
-
-        return bitmap
-    }
-
-    private fun saveMediaToStorage(bitmap: Bitmap) {
-        val filename = "${System.currentTimeMillis()}.jpg"
-        var fos: OutputStream? = null
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            this.contentResolver?.also { resolver ->
-                val contentValues = ContentValues().apply {
-                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-                }
-                val imageUri: android.net.Uri? =
-                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-                fos = imageUri?.let { resolver.openOutputStream(it) }
+            var calendarId = -1
+            if (cursor != null && cursor.moveToFirst()) {
+                calendarId = cursor.getInt(0)
+                cursor.close()
             }
-        } else {
-            val imagesDir =
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-            val image = File(imagesDir, filename)
-            fos = FileOutputStream(image)
+
+            // Guardar evento en el calendario predeterminado
+            val beginTime: Long = System.currentTimeMillis()
+            val endTime: Long = beginTime + 3600000
+            val timeZone: TimeZone = TimeZone.getDefault()
+
+            val values = ContentValues().apply {
+                put(CalendarContract.Events.TITLE, "Victoria en Piedra, Papel o Tijeras by Kotders")
+                put(CalendarContract.Events.DESCRIPTION, "¡Enorabuena, has ganado!")
+                put(CalendarContract.Events.DTSTART, beginTime)
+                put(CalendarContract.Events.DTEND, endTime)
+                put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.id)
+                put(CalendarContract.Events.CALENDAR_ID, calendarId)
+                put(CalendarContract.Events.HAS_ALARM, 1)
+            }
+
+            // Insertar evento en el calendario
+            val uri =
+                this.contentResolver.insert(CalendarContract.Events.CONTENT_URI, values)
+
+            if (uri != null) {
+                // Verificar que el evento se ha guardado en el calendario predeterminado
+                val eventProjection = arrayOf(CalendarContract.Events.CALENDAR_ID)
+                val eventCursor = this.contentResolver.query(
+                    uri, eventProjection, null, null, null
+                )
+
+                if (eventCursor != null && eventCursor.moveToFirst()) {
+                    val savedCalendarId = eventCursor.getInt(0)
+                    if (savedCalendarId == calendarId) {
+                        println("Evento guardado en el calendario predeterminado")
+                        println(uri.toString())
+                        Toast.makeText(this, "Evento guardado en el calendario", Toast.LENGTH_SHORT).show()
+                    } else {
+                        println("Error: el evento no se ha guardado en el calendario predeterminado")
+                        Toast.makeText(this, "Error: el evento no se ha guardado en el calendario", Toast.LENGTH_SHORT).show()
+                    }
+                    eventCursor.close()
+                }
+            } else {
+                println("Error guardando el evento en el calendario")
+                Toast.makeText(this, "Error guardando el evento en el calendario", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
-        fos?.use {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
-            Toast.makeText(
-                this@SolucionJuego,
-                "Captura realizada y guardada en la Galería",
-                Toast.LENGTH_SHORT
-            ).show()
+
+        buttonScreenshot.setOnClickListener {
+            takeScreenshot()
+            insertEventToCalendar()
         }
+
     }
 
     private fun logout() {
