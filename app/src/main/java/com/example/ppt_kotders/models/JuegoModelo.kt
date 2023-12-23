86729c019e056b640219c7165ace2bcf69d57a28
CiJuBar <101054409+CiJuBar@users.noreply.github.com>
2023-12-23 23:08:22 +0100

    recycle fix

--- app/src/main/java/com/example/ppt_kotders/models/JuegoModelo.kt
+++ app/src/main/java/com/example/ppt_kotders/models/JuegoModelo.kt
@@ -16,9 +16,5 @@ class JuegoModelo{
         this.latitud = latitud
     }
 
-    constructor(nombreJugador: String, resultado: String,fechahora:String){
-        this.nombreJugador = nombreJugador
-        this.resultado= resultado
-        this.fechahora = fechahora
-    }
+
 }
